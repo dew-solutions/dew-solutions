a4ed8ac42898751589938e6c482b6bcc4f53265e
Dayne Wagner <dewsolutionsowner@gmail.com>
2026-02-06 21:35:33 -0500

    Initial clean commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import ContactForm from '../components/ContactForm';
+
+export default function Home() {
+  return (
+    <main>
+      <h1>DEW Solutions</h1>
+      <ContactForm />
+    </main>
+  );
+}
